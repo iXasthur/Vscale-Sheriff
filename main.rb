a1c0427c50540d4cf70265570781b4d57d5d0f31
iXasthur <38611299+iXasthur@users.noreply.github.com>
2019-06-27 19:03:22 +0300

    +U1

--- main.rb
+++ main.rb
@@ -7,93 +7,89 @@ load 'createServerTool_Source.rb'
 load 'removalTool_Source.rb'
 load 'serverSyncTool_Source.rb'
 
-ACC_FILE = "AccountPreferences.txt"
+ACC_FILE = 'AccountPreferences.txt'.freeze
 TOKEN_LINE_IN_FILE = 0
-TOKEN = IO.readlines(ACC_FILE)[TOKEN_LINE_IN_FILE].chomp()
+TOKEN = IO.readlines(ACC_FILE)[TOKEN_LINE_IN_FILE].chomp
 
 PROGRAM_FOLDER_PATH = Dir.pwd
 # puts (Dir.entries(PROGRAM_FOLDER_PATH))
 
-$EMAIL = ""
+$EMAIL = ''
 
-$SERVER_NAMES_ARRAY = Array.new()
-$SERVER_CTID_ARRAY = Array.new()
+$SERVER_NAMES_ARRAY = Array[]
+$SERVER_CTID_ARRAY = Array[]
 
 
 rdyToExit = false
 menuIsShown = false
 
 
+# -----MAIN-----
 
-
-
-
-#############MAIN
-
-getInfo()
+getInfo
 
 
 # Main Menu
-while rdyToExit==false do
+while rdyToExit == false
 
-  if menuIsShown==false then
-    puts ("---Menu:\n1-Add Servers \n2-Remove Servers \n3-Sync Servers \n4-Server list\n0-Exit")
+  if menuIsShown == false
+    puts("---Menu:\n1-Add Servers \n2-Remove Servers \n3-Sync Servers \n4-Server list\n0-Exit")
     puts
     menuIsShown = true
   end
-  print (">")
+  print('>')
   # key = gets()
 
-  case gets().chomp()
+  case gets.chomp
   when '0'
-    rdyToExit=true
+    rdyToExit = true
   when '1'
-    puts ("Not available")
+    puts("Not available")
   when '2'
-    clearScreen()
-    printAccInfo()
+    clearScreen
+    printAccInfo
     menuIsShown = false
     puts
 
-    serverRemovalMenu()
+    serverRemovalMenu
 
-    clearScreen()
-    printAccInfo()
+    clearScreen
+    printAccInfo
     puts
   when '3'
-    clearScreen()
-    printAccInfo()
+    clearScreen
+    printAccInfo
     menuIsShown = false
 
-    puts ("\n>Syncing servers")
-    syncServers()
+    puts("\n>Syncing servers")
+    syncServers
 
 
-    clearScreen()
-    printAccInfo()
+    clearScreen
+    printAccInfo
     menuIsShown = false
 
-    puts ("\n>Syncing servers")
+    puts("\n>Syncing servers")
 
     puts
-    puts ("Found " + $SERVER_CTID_ARRAY.length().to_s() + " servers")
+    puts("Found " + $SERVER_CTID_ARRAY.length().to_s() + " servers")
     puts
     puts
     # puts (">Press [Enter] to continue")
     # gets
   when '4'
-    clearScreen()
-    printAccInfo()
+    clearScreen
+    printAccInfo
     menuIsShown = false
     puts
 
-    puts ("---Server list:")
-    for i in 0...$SERVER_CTID_ARRAY.length() do
-      puts ($SERVER_NAMES_ARRAY[i]+" (ctid: "+$SERVER_CTID_ARRAY[i] + ")")
+    puts("---Server list:")
+    for i in 0...$SERVER_CTID_ARRAY.length()
+      puts($SERVER_NAMES_ARRAY[i]+" (ctid: "+$SERVER_CTID_ARRAY[i] + ")")
     end
 
     puts
-    puts ("Server amount: " + $SERVER_CTID_ARRAY.length().to_s())
+    puts("Server amount: " + $SERVER_CTID_ARRAY.length().to_s())
     puts
     puts
   else
